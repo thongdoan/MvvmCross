0a2961e1bf1635028a989fc14b1197e150360e64
TH <bathong.doan@gmail.com>
2018-09-20 15:22:17 +0700

    - Wpf Network Plugin, call base.Load()

--- MvvmCross.Plugins/Network/Platforms/Wpf/Plugin.cs
+++ MvvmCross.Plugins/Network/Platforms/Wpf/Plugin.cs
@@ -12,6 +12,7 @@ namespace MvvmCross.Plugin.Network.Platforms.Wpf
     {
         public override void Load()
         {
+        	base.Load();
             Mvx.IoCProvider.RegisterType<IMvxReachability, MvxWpfReachability>();
         }
     }
